6b66119453b546f2c238aada59f899c9776b6700
github username <github email address>
2021-05-16 19:03:19 -0400

    commit tree 27b5b9cd65e322c683e8ffc22e0ba49e3cd23047

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,61 @@
 def create_an_empty_array
+    
+    []
   
 end
 
 def create_an_array
+
+    [1,2,3,4]
   
 end
 
 def add_element_to_end_of_array(array, element)
+
+    newArray = array.push(element)
+    newArray
+
   
 end
 
 def add_element_to_start_of_array(array, element)
+
+    newArray = array.unshift(element)
+    newArray
   
 end
 
 def remove_element_from_end_of_array(array)
+
+    newArray = array.pop
+    newArray
   
 end
 
 def remove_element_from_start_of_array(array)
+
+    newArray = array.shift
+    newArray
   
 end
 
 def retrieve_element_from_index(array, index_number)
+
+    requestedNumber = array[index_number]
+    requestedNumber
   
 end
 
 def retrieve_first_element_from_array(array)
+
+    requestedNumber = array[0]
+    requestedNumber
   
 end
 
 def retrieve_last_element_from_array(array)
+
+    requestedNumber = array[-1]
+    requestedNumber
   
 end
